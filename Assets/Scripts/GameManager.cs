bad2fe3c35a9f386cf8e5ca76c1bd68ac68cd25f
Eric Lee <ericslee@seas.upenn.edu>
2014-10-04 16:45:46 -0400

    Basic character movement (left, right, jump)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script will be used to manage the global game state
+
+public class GameManager : MonoBehaviour {
+
+	// Player
+	GameObject player;
+
+	// Prefabs
+	Object playerPrefab;
+	Object platformPrefab;
+	Object groundPrefab;
+
+	// Use this for initialization
+	void Start () {
+		// Cache references
+		playerPrefab = Resources.Load("Prefabs/Player");
+		platformPrefab = Resources.Load("Prefabs/Platform");
+		groundPrefab = Resources.Load("Prefabs/Ground");
+
+		// Instantiate player at start of level 1
+		player = (GameObject)Instantiate(playerPrefab);
+
+		SetUpLevel();
+	}
+	
+	void SetUpLevel() {
+		Instantiate(groundPrefab, new Vector3(0f, -3f, 0f), Quaternion.identity);
+
+		Instantiate(platformPrefab, new Vector3(-2f, 2f, 0f), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Controls
+	float distToGround;
+
+	// Use this for initialization
+	void Start () {
+		// get distance to ground
+		distToGround = collider.bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		HandleMovement();
+	}
+
+	void HandleMovement() {
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			transform.Translate(-Vector2.right * 4f * Time.deltaTime);
+		}
+
+		// Jump
+		if (Input.GetKeyDown (KeyCode.Space) && IsGrounded()) {
+			rigidbody.velocity = new Vector3(0, 8, 0);
+		}
+	}
+
+	bool IsGrounded() {
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+	}
+}
