bf51794777da47a3506da1392f7c890ef0337708
Eric Lee <ericslee@seas.upenn.edu>
2014-10-05 20:35:22 -0400

    Created very basic first level for testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour {
 	Object playerPrefab;
 	Object platformPrefab;
 	Object groundPrefab;
+    Object wallPrefab;
 
 	// Use this for initialization
 	void Start () 
@@ -20,6 +21,7 @@ public class GameManager : MonoBehaviour {
 		playerPrefab = Resources.Load("Prefabs/Player");
 		platformPrefab = Resources.Load("Prefabs/Platform");
 		groundPrefab = Resources.Load("Prefabs/Ground");
+        wallPrefab = Resources.Load("Prefabs/Wall");
 
 		// Instantiate player at start of level 1
 		player = (GameObject)Instantiate(playerPrefab);
@@ -29,9 +31,21 @@ public class GameManager : MonoBehaviour {
 	
 	void SetUpLevel() 
     {
+        ///////////////////// First level /////////////////////
 		Instantiate(groundPrefab, new Vector3(0f, -7f, 0f), Quaternion.identity);
-   
+        Instantiate(wallPrefab, new Vector3(-20f, 0f, 0f), Quaternion.identity);
+        Instantiate(wallPrefab, new Vector3(20f, 0f, 0f), Quaternion.identity);
+
 		Instantiate(platformPrefab, new Vector3(-2f, 0.2f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(5f, 3.5f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(-2f, 6f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(5f, 9f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(-2f, 12f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(5f, 15f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(-2f, 18f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(5f, 21f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(-2f, 24f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(5f, 27f, 0f), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
