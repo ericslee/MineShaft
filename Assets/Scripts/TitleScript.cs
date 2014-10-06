537a278e99149a3e44104e166bf7c12750e42f0d
cohensam <cohensam@seas.upenn.edu>
2014-10-06 00:40:00 -0400

    Added first iteration of all 5 levels, main menu, win, lose, and instruction scenes.

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+	
+	private GUIStyle buttonStyle; 
+	// Use this for initialization 
+	void Start () {
+		
+	} 
+	
+	// Update is called once per frame 
+	void Update () { 
+		
+	} 
+	
+	void OnGUI (){ 
+		GUILayout.BeginArea(new Rect(425, 150, 
+		                             175, 425)); 
+		GUILayout.TextField("MineShaft");
+		// Load the main scene 
+		// The scene needs to be added into build setting to be loaded! 
+		if (GUILayout.Button("New Game")) 
+		{ 
+			Application.LoadLevel("Level1"); 
+		}
+		
+		if (GUILayout.Button ("Instructions")) {
+			Application.LoadLevel("Instructions");
+		}
+		
+		if (GUILayout.Button("Exit")) 
+		{ 
+			Application.Quit(); 
+			Debug.Log ("Application.Quit() only works in build, not in editor"); 
+		} 
+		
+		GUILayout.EndArea(); 
+	} 
+}
