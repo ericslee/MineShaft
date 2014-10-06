a2c4e82fe1fb10b10c886ae3ebe749c19c30af29
Eric Lee <ericslee@seas.upenn.edu>
2014-10-06 17:41:21 -0400

    Displaying current level in text

---
+++ Assets/Scripts/GameHUD.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHUD : MonoBehaviour
+{
+    GameManager gameManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        // cache references
+        gameManager = GetComponent<GameManager>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 5, Screen.width / 5, Screen.height / 25), "Current Level: " + gameManager.GetCurrentLevel());
+    }
+}
