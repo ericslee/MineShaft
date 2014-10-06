f2f5ceaf44a4b952e81be0aa0b27deddd5a8b801
Eric Lee <ericslee@seas.upenn.edu>
2014-10-06 11:55:27 -0400

    Fixing formatting

--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -4,29 +4,30 @@ using System.Collections;
 /*
  *  Script used to transition to a new level and move the camera accordingly when the object this script is attached to is crossed  
  */
-public class LevelControl : MonoBehaviour {
+public class LevelControl : MonoBehaviour
+{
 
     // level control
     int correspondingLevel; // level that the game should switch to when this threshold is crossed
 
     GameManager gameManager;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
-	    // cache references
+        // cache references
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    }
+    
+    // Update is called once per frame
+    void Update()
     {
 
-	}
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (correspondingLevel != gameManager.GetCurrentLevel() && other.tag.Equals("Player")) 
+        if (correspondingLevel != gameManager.GetCurrentLevel() && other.tag.Equals("Player"))
         {
             // increment the level if entering from below
             gameManager.ChangeLevel(correspondingLevel);
@@ -37,7 +38,7 @@ public class LevelControl : MonoBehaviour {
      *  Getters and setters
      */ 
 
-    public void SetCorrespondingLevel(int level) 
+    public void SetCorrespondingLevel(int level)
     {
         correspondingLevel = level;
     }
