b4b11872abff7c6bf18c1d61dd093ec42d732a66
Eric Lee <ericslee@seas.upenn.edu>
2014-10-05 20:41:06 -0400

    Fixing code formatting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,40 +3,41 @@ using System.Collections;
 
 // This script will be used to manage the global game state
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Player
-	GameObject player;
+    // Player
+    GameObject player;
 
-	// Prefabs
-	Object playerPrefab;
-	Object platformPrefab;
-	Object groundPrefab;
+    // Prefabs
+    Object playerPrefab;
+    Object platformPrefab;
+    Object groundPrefab;
     Object wallPrefab;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
-		// Cache references
-		playerPrefab = Resources.Load("Prefabs/Player");
-		platformPrefab = Resources.Load("Prefabs/Platform");
-		groundPrefab = Resources.Load("Prefabs/Ground");
+        // Cache references
+        playerPrefab = Resources.Load("Prefabs/Player");
+        platformPrefab = Resources.Load("Prefabs/Platform");
+        groundPrefab = Resources.Load("Prefabs/Ground");
         wallPrefab = Resources.Load("Prefabs/Wall");
 
-		// Instantiate player at start of level 1
-		player = (GameObject)Instantiate(playerPrefab);
+        // Instantiate player at start of level 1
+        player = (GameObject)Instantiate(playerPrefab);
 
-		SetUpLevel();
-	}
-	
-	void SetUpLevel() 
+        SetUpLevel();
+    }
+    
+    void SetUpLevel()
     {
         ///////////////////// First level /////////////////////
-		Instantiate(groundPrefab, new Vector3(0f, -7f, 0f), Quaternion.identity);
+        Instantiate(groundPrefab, new Vector3(0f, -7f, 0f), Quaternion.identity);
         Instantiate(wallPrefab, new Vector3(-20f, 0f, 0f), Quaternion.identity);
         Instantiate(wallPrefab, new Vector3(20f, 0f, 0f), Quaternion.identity);
 
-		Instantiate(platformPrefab, new Vector3(-2f, 0.2f, 0f), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(-2f, 0.2f, 0f), Quaternion.identity);
         Instantiate(platformPrefab, new Vector3(5f, 3.5f, 0f), Quaternion.identity);
         Instantiate(platformPrefab, new Vector3(-2f, 6f, 0f), Quaternion.identity);
         Instantiate(platformPrefab, new Vector3(5f, 9f, 0f), Quaternion.identity);
@@ -46,11 +47,14 @@ public class GameManager : MonoBehaviour {
         Instantiate(platformPrefab, new Vector3(5f, 21f, 0f), Quaternion.identity);
         Instantiate(platformPrefab, new Vector3(-2f, 24f, 0f), Quaternion.identity);
         Instantiate(platformPrefab, new Vector3(5f, 27f, 0f), Quaternion.identity);
-	}
-	
-	// Update is called once per frame
-	void Update () 
+
+        ///////////////////// Second level /////////////////////
+
+    }
+    
+    // Update is called once per frame
+    void Update()
     {
-	
-	}
+    
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,66 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Controls
-	float distToGround;
-	bool collidingWall; // used for disabling left, right controls when colliding with a wall
+    // Controls
+    float distToGround;
+    bool collidingWall; // used for disabling left, right controls when colliding with a wall
 
-	// Use this for initialization
-	void Start() 
+    // Use this for initialization
+    void Start()
     {
-		// get distance to ground
-		distToGround = collider.bounds.extents.y;
-	}
+        // get distance to ground
+        distToGround = collider.bounds.extents.y;
+    }
 
-	// Update is called once per frame
-	void Update() 
+    // Update is called once per frame
+    void Update()
     {
         HandleMovement();
-	}
+    }
 
-	void HandleMovement() 
+    void HandleMovement()
     {
-		if (!collidingWall) {
-			if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-				transform.Translate(Vector2.right * 4f * Time.deltaTime);
-			}
-			if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-				transform.Translate(-Vector2.right * 4f * Time.deltaTime);
-			}
-		}
+        if (!collidingWall)
+        {
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-Vector2.right * 4f * Time.deltaTime);
+            }
+        }
 
-		// Jump
-		if (IsGrounded()) {
-			collidingWall = false;
-			if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
-				rigidbody.velocity = new Vector3(0, 8, 0);
-			}
-		}
-	}
+        // Jump
+        if (IsGrounded())
+        {
+            collidingWall = false;
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rigidbody.velocity = new Vector3(0, 8, 0);
+            }
+        }
+    }
 
     // checks if player is grounded with three tiny raycasts from the left bound, center, and right bound of the collider
-	bool IsGrounded() 
+    bool IsGrounded()
     {
         Vector3 leftBound = new Vector3(transform.position.x - (GetComponent<BoxCollider>().size.x / 2), transform.position.y, transform.position.z);
         Vector3 rightBound = new Vector3(transform.position.x + (GetComponent<BoxCollider>().size.x / 2), transform.position.y, transform.position.z);
 
-		return (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f)
-                || (Physics.Raycast(leftBound, -Vector3.up, distToGround + 0.1f))
-                || (Physics.Raycast(rightBound, -Vector3.up, distToGround + 0.1f)));
-	}
+        return (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f)
+            || (Physics.Raycast(leftBound, -Vector3.up, distToGround + 0.1f))
+            || (Physics.Raycast(rightBound, -Vector3.up, distToGround + 0.1f)));
+    }
 
-	void OnCollisionEnter(Collision collision) 
+    void OnCollisionEnter(Collision collision)
     { 
-		// environment tag needed in case we want to be able to control 
-		if (collision.gameObject.tag.Equals("Environment") && !IsGrounded()) {
-			collidingWall = true;
-		}
-	}
+        // environment tag needed in case we want to be able to control 
+        if (collision.gameObject.tag.Equals("Environment") && !IsGrounded())
+        {
+            collidingWall = true;
+        }
+    }
 
-	void OnCollisionExit() 
+    void OnCollisionExit()
     {
-		collidingWall = false;
-	}
+        collidingWall = false;
+    }
 }
