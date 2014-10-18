cb88266cce42abda9e6cc8f4a1561d8940a5a8b1
Jeremy <jeremymnewlin@gmail.com>
2014-10-18 15:09:04 -0400

    gravity gun upated with new assets and logic

---
+++ Assets/Scripts/GravityCenterScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityCenterScript : MonoBehaviour {
+
+	public Transform gravityPlane;
+	float gravityPlaneRotateX;
+	Vector2 gravityPlaneMinMaxScale;
+	float gravityAbsMaxScale;
+	float gravityPlaneScale;
+
+	// Use this for initialization
+	void Start () {
+		gravityPlaneRotateX = 0;
+		gravityPlaneMinMaxScale = new Vector2(0,1.0f);
+		gravityAbsMaxScale = 1;
+		gravityPlaneScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gravityPlaneRotateX += 0.025f;
+		gravityPlane.Rotate(new Vector3(0, gravityPlaneRotateX, 0));
+
+		if (gravityPlaneScale > gravityPlaneMinMaxScale.y){
+			gravityPlaneScale = gravityPlaneMinMaxScale.x;
+			gravityPlaneRotateX = 0;
+		}
+		gravityPlaneScale += 0.025f;
+
+		gravityPlane.localScale = new Vector3(gravityPlaneScale,gravityPlaneScale,gravityPlaneScale);
+	}
+
+	public void SetGravityPlaneMax(float newMax){
+		Debug.Log(newMax);
+		gravityPlaneMinMaxScale.y = newMax;
+		Debug.Log(gravityPlaneMinMaxScale.y);
+	}
+
+	public float GetAbsMaxScale(){
+		return gravityAbsMaxScale;
+	}
+}
---
+++ Assets/Scripts/GravityPlaneScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityPlaneScript : MonoBehaviour {
+
+	GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Platform"){
+			gameManager.GetActivePlayer().AddToGravityList(other.gameObject);
+		}
+
+	}
+}
